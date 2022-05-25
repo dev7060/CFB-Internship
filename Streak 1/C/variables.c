bd20389402872eeb77e7b6636048a87a37e30edd
Dev <46674147+dev7060@users.noreply.github.com>
2022-05-25 14:47:32 +0530

    Update variables.c

--- Streak 1/C/variables.c
+++ Streak 1/C/variables.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a = 4;
-    //int b=8.5; //not recommended
+    // int b=8.5; //not recommended
     float b = 8.5;
     char u = 'u';
+    int d = 45;
+    int e = 5 + 2;
     printf("the value of a is %d\n", a);
     printf("the value of b is %f", b);
-
-    // printf("Hello World!");
+    printf("the value of a+d is %f", a + d);
     return 0;
 }
