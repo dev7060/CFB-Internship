0018a15c964c523395bc21a49fab26347fefe1e6
Dev <46674147+dev7060@users.noreply.github.com>
2022-05-25 14:42:58 +0530

    Create variables.c

---
+++ Streak 1/C/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 4;
+    //int b=8.5; //not recommended
+    float b = 8.5;
+    char u = 'u';
+    printf("the value of a is %d\n", a);
+    printf("the value of b is %f", b);
+
+    // printf("Hello World!");
+    return 0;
+}
