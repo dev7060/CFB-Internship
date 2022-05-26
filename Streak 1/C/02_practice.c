d494d097eb51ebd0d242140b73bd640399a0cf5c
Dev <46674147+dev7060@users.noreply.github.com>
2022-05-26 23:31:37 +0530

    Update 02_practice.c

--- Streak 1/C/02_practice.c
+++ Streak 1/C/02_practice.c
@@ -4,6 +4,8 @@ int main()
 {
     int r = 3;
     float pi = 3.14;
-    printf("area is: %f", pi*r*r);
+    printf("area is: %f\n", pi * r * r);
+    int height = 3;
+    printf("volume of cylinder is %f", pi * r * r * height);
     return 0;
 }
