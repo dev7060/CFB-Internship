dfdd3be81c052c3c05c8cac3ce3e27744814ac0d
Dev <46674147+dev7060@users.noreply.github.com>
2022-06-28 09:31:52 +0530

    Add files via upload

--- Streak 1/C/02_practice.c
+++ Streak 1/C/02_practice.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int r = 3;
+    int radius = 3;
     float pi = 3.14;
-    printf("area is: %f\n", pi * r * r);
+    printf("The area of this circle is %f\n", pi * radius * radius);
     int height = 3;
-    printf("volume of cylinder is %f", pi * r * r * height);
+    printf("Volume of this cylinder is %f\n", pi * radius * radius * height);
     return 0;
 }
